bf025752959702d46b857decff2ef0db13200206
Sourav Kumar <souravkumar4997@gmail.com>
2023-07-06 10:46:13 +0530

    Refactor createPost method and moved all the logic to a private funtions.

--- src/main/java/com/example/springbootblogrestapi/service/impl/PostServiceImpl.java
+++ src/main/java/com/example/springbootblogrestapi/service/impl/PostServiceImpl.java
@@ -15,21 +15,27 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public PostDTO createPost(PostDTO postDTO) {
-        // convert DTO to entity
+        Post post = mapToEntity(postDTO);
+        Post newPost = postRepository.save(post);
+        return mapToDto(newPost);
+    }
+
+    // Convert Entity into DTO
+    private PostDTO mapToDto(Post post){
+        PostDTO postDTO = new PostDTO();
+        postDTO.setId(post.getId());
+        postDTO.setTitle(post.getTitle());
+        postDTO.setDescription(post.getDescription());
+        postDTO.setContent(post.getContent());
+        return postDTO;
+    }
+
+    // Convert DTO into Entity
+    private Post mapToEntity(PostDTO postDTO){
         Post post = new Post();
         post.setTitle(postDTO.getTitle());
         post.setDescription(postDTO.getDescription());
         post.setContent(postDTO.getContent());
-
-        Post newPost = postRepository.save(post);
-
-        // convert entity to DTO
-        PostDTO postResponse = new PostDTO();
-        postResponse.setId(newPost.getId());
-        postResponse.setTitle(newPost.getTitle());
-        postResponse.setDescription(newPost.getDescription());
-        postResponse.setContent(newPost.getContent());
-
-        return postResponse;
+        return post;
     }
 }
